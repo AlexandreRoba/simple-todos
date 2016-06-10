420c33f97c998cbcd704236f73554b088719075e
Alexandre Roba <alexandre.roba@arobanet.be>
2016-06-10 15:52:54 +0200

    Added the form to capture the task and sort the result by created date'

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -4,8 +4,25 @@ import {Tasks} from '../api/tasks.js';
 
 import './body.html';
 
+//Reminder here we create an json object for wich the properties tasks is a function
+//It is using the Object Literal Extensions of ES6
 Template.body.helpers({
     tasks(){
-        return Tasks.find({});
+        return Tasks.find({},{sort:{createdAt:-1}});
+    }
+});
+
+Template.body.events({
+    'submit .new-task'(event){
+        //prefent default browser form submit
+        event.preventDefault();
+
+        const target = event.target;
+        const text = target.text.value;
+
+        //Insert a task into the collection
+        Tasks.insert({text:text,createdAt:new Date()});
+
+        target.text.value='';
     }
 });
