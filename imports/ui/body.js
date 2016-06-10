a1c4e7df655a826c1b1a4deac8c606e7bf4d91ae
Alexandre Roba <alexandre.roba@arobanet.be>
2016-06-10 18:01:26 +0200

    Added a filter to hide the completed tasks

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,4 +1,5 @@
 import {Template} from 'meteor/templating';
+import {ReactiveDict} from 'meteor/reactive-dict';
 
 import {Tasks} from '../api/tasks.js';
 
@@ -6,12 +7,23 @@ import './task.js';
 import './body.html';
 
 
+Template.body.onCreated(function onCreated(){
+    this.state = new ReactiveDict();
+});
+
 //Reminder here we create an json object for wich the properties tasks is a function
 //It is using the Object Literal Extensions of ES6
 Template.body.helpers({
     tasks(){
+        const instance = Template.instance();
+        if (instance.state.get('hideCompleted')) {
+            return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+        }
         return Tasks.find({},{sort:{createdAt:-1}});
-    }
+    },
+    incompleteCount() {
+        return Tasks.find({ checked: { $ne: true } }).count();
+    },
 });
 
 Template.body.events({
@@ -26,5 +38,9 @@ Template.body.events({
         Tasks.insert({text:text,createdAt:new Date()});
 
         target.text.value='';
-    }
+    },
+    'change .hide-completed input'(event, instance) {
+        instance.state.set('hideCompleted', event.target.checked);
+    },
+    
 });
